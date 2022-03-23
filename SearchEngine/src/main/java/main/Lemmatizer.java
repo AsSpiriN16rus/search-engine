47afa02963262bb97d9d350afb3db692defc4fb8
AsSpiriN16rus <bygaga357@yandex.ru>
2022-03-23 17:09:39 +0300

    4stage

--- SearchEngine/src/main/java/main/Lemmatizer.java
+++ SearchEngine/src/main/java/main/Lemmatizer.java
@@ -1,86 +1,87 @@
-package main;
-
-import liquibase.pro.packaged.S;
-import main.model.Index;
-import org.apache.lucene.morphology.LuceneMorphology;
-import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-
-
-public class Lemmatizer
-{
-    private static DataBase dataBase;
-
-    public static void lemRank(HashMap documentWeight, String url){
-        HashMap<String, Double> rankText = new HashMap();
-        for (Object name: documentWeight.keySet()){
-            String key = name.toString();
-            Double weight = Double.valueOf(documentWeight.get(name).toString());
-            HashMap lemValue = lemmatizerText(key, false);
-            for (Object qw : lemValue.keySet()){
-                Double value = Double.valueOf(lemValue.get(qw).toString());
-                if (rankText.containsKey(qw.toString())){
-                    Double valueMap = Double.valueOf(rankText.get(qw));
-                    rankText.put(qw.toString(), (value * weight) + valueMap);
-                }else {
-                    rankText.put(qw.toString(), value * weight);
-                }
-            }
-        }
-
-        for (Object name: rankText.keySet()){
-            Float weight = Float.valueOf(rankText.get(name).toString());
-            Index index = new Index();
-            index.setLemmaId(dataBase.getLemmaId(name.toString()));
-            index.setRank(weight);
-            index.setPageId(dataBase.getPageId(url));
-            dataBase.addIndex(index);
-        }
-
-
-    }
-
-    public static HashMap<String, Integer> lemmatizerText(String textLem , Boolean fullText){
-        dataBase = new DataBase();
-        HashMap<String,Integer> luceneMap = new HashMap<>();
-        try {
-
-            LuceneMorphology luceneMorphology = new RussianLuceneMorphology();
-            String[] textSplit = textLem.replaceAll("[^а-яА-ЯёЁ ]", " ").toLowerCase().split("\\s+");
-            for (String string : textSplit){
-                if (string.length() == 0){
-                    continue;
-                }
-                boolean textValid = true;
-                List<String> wordBaseForms = luceneMorphology.getMorphInfo(string);
-                for (String morphInfo : wordBaseForms){
-                    String morphInfoSplit[] = morphInfo.split("\\s+");
-                    String lastWord = morphInfoSplit[morphInfoSplit.length - 1];
-                    if (lastWord.equals("ПРЕДЛ") || lastWord.equals("МЕЖД") || lastWord.equals("СОЮЗ") || lastWord.equals("ЧАСТ")){
-                        textValid = false;
-                        break;
-                    }
-                }
-                if (textValid){
-                    List<String> wordBase = luceneMorphology.getNormalForms(string);
-                    for (String s : wordBase){
-                        if (luceneMap.containsKey(s)){
-                            luceneMap.put(s,luceneMap.get(s) + 1);
-                        }else {
-                            luceneMap.put(s,1);
-                        }
-                    }
-                }
-            }
-            if (fullText == true) {
-                dataBase.outputMap(luceneMap);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return luceneMap;
-    }
-}
+//package main;
+//
+//import liquibase.pro.packaged.S;
+//import main.model.Index;
+//import org.apache.lucene.morphology.LuceneMorphology;
+//import org.apache.lucene.morphology.russian.RussianLuceneMorphology;
+//
+//import java.io.IOException;
+//import java.util.HashMap;
+//import java.util.List;
+//
+//import static main.DataBase.dataBase;
+//
+//
+//public class Lemmatizer
+//{
+//
+//    public static void lemRank(HashMap documentWeight, String url){
+//        HashMap<String, Double> rankText = new HashMap();
+//        for (Object name: documentWeight.keySet()){
+//            String key = name.toString();
+//            Double weight = Double.valueOf(documentWeight.get(name).toString());
+//            HashMap lemValue = lemmatizerText(key, false);
+//            for (Object qw : lemValue.keySet()){
+//                Double value = Double.valueOf(lemValue.get(qw).toString());
+//                if (rankText.containsKey(qw.toString())){
+//                    Double valueMap = Double.valueOf(rankText.get(qw));
+//                    rankText.put(qw.toString(), (value * weight) + valueMap);
+//                }else {
+//                    rankText.put(qw.toString(), value * weight);
+//                }
+//            }
+//        }
+//
+//        for (Object name: rankText.keySet()){
+//            Float weight = Float.valueOf(rankText.get(name).toString());
+//            Index index = new Index();
+//            index.setLemmaId(dataBase.getLemmaId(name.toString()));
+//            index.setRank(weight);
+//            index.setPageId(dataBase.getPageId(url));
+//            dataBase.addIndex(index);
+//        }
+//
+//
+//    }
+//
+//    public static HashMap<String, Integer> lemmatizerText(String textLem , Boolean fullText){
+//        dataBase = new DataBase();
+//        HashMap<String,Integer> luceneMap = new HashMap<>();
+//        try {
+//
+//            LuceneMorphology luceneMorphology = new RussianLuceneMorphology();
+//            String[] textSplit = textLem.replaceAll("[^а-яА-ЯёЁ ]", " ").toLowerCase().split("\\s+");
+//            for (String string : textSplit){
+//                if (string.length() == 0){
+//                    continue;
+//                }
+//                boolean textValid = true;
+//                List<String> wordBaseForms = luceneMorphology.getMorphInfo(string);
+//                for (String morphInfo : wordBaseForms){
+//                    String morphInfoSplit[] = morphInfo.split("\\s+");
+//                    String lastWord = morphInfoSplit[morphInfoSplit.length - 1];
+//                    if (lastWord.equals("ПРЕДЛ") || lastWord.equals("МЕЖД") || lastWord.equals("СОЮЗ") || lastWord.equals("ЧАСТ")){
+//                        textValid = false;
+//                        break;
+//                    }
+//                }
+//                if (textValid){
+//                    List<String> wordBase = luceneMorphology.getNormalForms(string);
+//                    for (String s : wordBase){
+//                        if (luceneMap.containsKey(s)){
+//                            luceneMap.put(s,luceneMap.get(s) + 1);
+//                        }else {
+//                            luceneMap.put(s,1);
+//                        }
+//                    }
+//                }
+//            }
+//            if (fullText == true) {
+//                dataBase.outputMap(luceneMap);
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return luceneMap;
+//    }
+//}
--- SearchEngine/src/main/java/main/SiteCrawling.java
+++ SearchEngine/src/main/java/main/SiteCrawling.java
@@ -1,6 +1,5 @@
 package main;
 
-import liquibase.pro.packaged.D;
 import main.model.Field;
 import main.model.Page;
 import org.jsoup.Jsoup;
@@ -9,47 +8,64 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Repository;
 
-import javax.sql.DataSource;
 import java.util.*;
 import java.util.concurrent.RecursiveAction;
 
+@Repository
 public class SiteCrawling  extends RecursiveAction
 {
     private String url;
     private static String urlOne;
     private static int nestingСounter = 0;
     private static Set<String> siteUrlListAfter = new HashSet<>();
+    public SiteCrawling(){}
     public SiteCrawling(String url) {
         this.url = url;
     }
-    private static DataBase dataBase = new DataBase();
+//    public static DataBase dataBase = new DataBase();
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+//    @Autowired
+//    public void setDataSource(DataSource dataSource){
+//        this.jdbcTemplate = new JdbcTemplate(dataSource);
+//
+//    }
 
+//    public static DataSource mysqlDataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/search_engine?createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true&useSSL=false&useUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("q1w2e3r4t5A1");
+//        return dataSource;
+//    }
 
-    public void lemText(Document document, String url){
-        StringBuilder stringBuilder = new StringBuilder();
-        HashMap <String,Double> documentWeight = new HashMap<>();
-        for (Field tag :  dataBase.findAllField()){
-
-            String tagText = document.select(tag.getSelector()).text();
-            documentWeight.put(tagText, tag.getWeight());
-            stringBuilder.append(tagText + " ");
-        }
-
-
-        Lemmatizer.lemmatizerText(stringBuilder.toString(), true);
-        Lemmatizer.lemRank(documentWeight, url);
-    }
+//    public void lemText(Document document, String url){
+//        StringBuilder stringBuilder = new StringBuilder();
+//        HashMap <String,Double> documentWeight = new HashMap<>();
+//        for (Field tag :  findAllField()){
+//
+//            String tagText = document.select(tag.getSelector()).text();
+//            documentWeight.put(tagText, tag.getWeight());
+//            stringBuilder.append(tagText + " ");
+//        }
+//
+//
+////        Lemmatizer.lemmatizerText(stringBuilder.toString(), true);
+////        Lemmatizer.lemRank(documentWeight, url);
+//    }
 
     @Override
     protected void compute() {
         try {
+//            setDataSource(mysqlDataSource());
             if (nestingСounter == 0 ) {
                 nestingСounter = 1;
                 urlOne = url;
-                dataBase.dropTable();
+                dropTable();
             }
 
             List<SiteCrawling> tasks =  new ArrayList<>();
@@ -66,7 +82,7 @@ public class SiteCrawling  extends RecursiveAction
             org.jsoup.Connection.Response statusCode = Jsoup.connect(url).execute();
             Page link = new Page(href,statusCode.statusCode(),document.toString()) ;
             siteUrlListAfter.add(url);
-            dataBase.addField(link);
+            addField(link);
 
 
             Elements element = document.select("a");
@@ -82,8 +98,8 @@ public class SiteCrawling  extends RecursiveAction
 
             }
 
-
-            lemText(document,href);
+//
+//            lemText(document,href);
 
             for (SiteCrawling item : tasks){
                 item.join();
@@ -95,6 +111,37 @@ public class SiteCrawling  extends RecursiveAction
             ex.printStackTrace();
         }
     }
+
+
+    public void addField(Page link){
+        jdbcTemplate.update("INSERT IGNORE INTO page(path, code,content) VALUES(?,?,?)",
+                link.getPath(),link.getCode(),link.getContent());
+    }
+
+    public void dropTable(){
+        String sql = "TRUNCATE lemma";
+        jdbcTemplate.update(sql);
+        sql = "TRUNCATE page";
+        jdbcTemplate.update(sql);
+        sql = "TRUNCATE `index`";
+        jdbcTemplate.update(sql);
+    }
+
+        public List<Field> findAllField() {
+
+            String sql = "SELECT * FROM search_engine.field";
+            List<Field> fields = jdbcTemplate.query(
+                    sql,
+                    (rs, rowNum) ->
+                            new Field(
+                                    rs.getInt("id"),
+                                    rs.getString("name"),
+                                    rs.getString("selector"),
+                                    rs.getDouble("weight")
+                            )
+            );
+        return fields;
+        }
 
 
 }
